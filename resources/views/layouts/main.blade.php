26130dcf514a8e0abf630abefd38ce06cbba72f6
Mohammad Rangga Irghivya <mohammadranggairghivya@gmail.com>
2026-02-04 15:35:41 +0700

    Pemesanan

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,4 +9,5 @@
 <body>
     @yield('content')
 </body>
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Public User
+
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/keranjang', function() {
+    return view('keranjang');
+})->name('keranjang');
